915b0345b3d4fcde8e20f9c1d2471aecc7c9fdda
Nastia <dnsnastia@gmail.com>
2022-04-10 18:29:15 +0300

    Made a contact page in tailwind style

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,25 @@
 // rsc
 
 import React from 'react';
-
+ // import "tailwindcss/tailwind.css";
 
 const AboutUs = () => {
     return (
         <div>
-            About Us!
+        <button className="btn btn-primary">Button</button>
+            <div tabIndex="0" className="collapse group">
+                <div
+                    className="collapse-title bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+                    Focus me to see content
+                </div>
+                <div
+                    className="collapse-content bg-primary text-primary-content group-focus:bg-secondary group-focus:text-secondary-content">
+                    <p>tabindex="0" attribute is necessary to make the div focusable</p>
+                </div>
+            </div>
+
+
+            <a className="link link-secondary">I'm a simple link</a>
         </div>
     );
 };
